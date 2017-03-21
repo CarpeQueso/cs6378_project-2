c36f62aaee99e419ce50a65648c3a7b14c0edf60
J. Andrew Crain <andrew.crain1@gmail.com>
2017-03-21 13:07:24 -0500

    Added synchronization to VectorClock so it can be used by multiple threads

--- src/VectorClock.java
+++ src/VectorClock.java
@@ -17,7 +17,13 @@ public class VectorClock {
 		System.arraycopy(initialValues, 0, this.clock, 0, size);
 	}
 
-	public void increment(int index) {
+	public VectorClock(VectorClock vc) {
+		this.size = vc.getSize();
+		this.clock = new int[size];
+		System.arraycopy(vc.getClockVector(), 0, this.clock, 0, size);
+	}
+
+	public synchronized void increment(int index) {
 		assert index >= 0 && index < size;
 
 		clock[index]++;
@@ -27,7 +33,7 @@ public class VectorClock {
 	 * Update this vector clock based on the given vector clock.
 	 * The maximum value for a given position will be stored in this clock.
 	 */
-	public void update(VectorClock vc) {
+	public synchronized void update(VectorClock vc) {
 		assert vc.getSize() == this.size;
 		int[] passedClockVector = vc.getClockVector();
 
@@ -38,7 +44,7 @@ public class VectorClock {
 		}
 	}
 
-	public VectorComparisonType compareTo(VectorClock vc) {
+	public synchronized VectorComparisonType compareTo(VectorClock vc) {
 		if (this.size != vc.getSize()) {
 			return VectorComparisonType.INCOMPARABLE;
 		}
@@ -67,11 +73,11 @@ public class VectorClock {
 		return tentativeComparisonType; 
 	}
 
-	public int getClockValueAtIndex(int index) {
+	public synchronized int getClockValueAtIndex(int index) {
 		return this.clock[index];
 	}
 
-	public int[] getClockVector() {
+	public synchronized int[] getClockVector() {
 		int[] clockCopy = new int[this.size];
 
 		System.arraycopy(clock, 0, clockCopy, 0, this.size);
@@ -82,7 +88,7 @@ public class VectorClock {
 		return size;
 	}
 
-	public String toString() {
+	public synchronized String toString() {
 		StringBuilder sb = new StringBuilder(this.size * 2);
 
 		sb.append(clock[0]);
